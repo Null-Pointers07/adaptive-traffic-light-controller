0357864e278b2c5df7560f8ec4f6e25429e693cf
mominbhati-007 <mominbhati007@gmail.com>
2026-08-01 13:26:23 +0530

    Add ALU module implementation

---
+++ src/alu.v
@@ -0,0 +1,11 @@
+module alu (input [5:0] density,
+            output reg [5:0] duration)
+    always @(*) begin
+    if(density<=20)
+    duration = 30;
+    else if (density<=40)
+    duration = 45;
+    else if (density>40)
+    duration = 60;
+    end
+endmodule
